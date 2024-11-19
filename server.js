40b074f1d48085ba5e34c0bc21a13045762ff9d4
Kaddyp <kaddyp2805@gmail.com>
2024-11-19 00:20:22 +0000

    second commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', (req, res) => {
     res.json({
         status: 200,
-        message: "Node Js Docker deployment"
+        message: "Node Js Docker deployment in Dockerhub and Ec2"
     })
 })
 
